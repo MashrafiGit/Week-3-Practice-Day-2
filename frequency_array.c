8761a0ca124db682eafb63a1d95534defe25e959
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-24 19:14:38 +0600

    frequency array with shortcut and dynamic also

--- frequency_array.c
+++ frequency_array.c
@@ -11,23 +11,18 @@ int main()
         scanf("%d", &a[i]);
     }
 
-    // int fre0 = 0, fre1 = 0, fre2 = 0, fre3 = 0, fre4 = 0, fre5 = 0, fre6 = 0;
-
     int fre[6] = {0};
 
     for (int i = 0; i < n; i++)
     {
-        fre[a[i]];
+        fre[a[i]]++;
     }
 
-    printf("%d -> %d\n", 0, fre[0]);
-    printf("%d -> %d\n", 1, fre[1]);
-    printf("%d -> %d\n", 2, fre[2]);
-    printf("%d -> %d\n", 3, fre[3]);
-    printf("%d -> %d\n", 4, fre[4]);
-    printf("%d -> %d\n", 5, fre[5]);
-    printf("%d -> %d\n", 6, fre[6]);
 
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d -> %d\n", i, fre[i]);
+    }
 
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+  printf("%d -> %d\n", 0, fre[0]);
+    printf("%d -> %d\n", 1, fre[1]);
+    printf("%d -> %d\n", 2, fre[2]);
+    printf("%d -> %d\n", 3, fre[3]);
+    printf("%d -> %d\n", 4, fre[4]);
+    printf("%d -> %d\n", 5, fre[5]);
+    printf("%d -> %d\n", 6, fre[6]);
