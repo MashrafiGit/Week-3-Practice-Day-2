11b16c655fe0d9bf411a0bd1f69e2e749eb2e38d
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-25 02:04:17 +0600

    sum digit from a string

---
+++ K_Sum_Digits.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    char a[n];
+    scanf("%s", &a);
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + a[i] - 48;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
